f9a3d894a0ce83896f3658b643d4f9545aca9a26
pallavi bhattarai <67225012+pallavi000@users.noreply.github.com>
2023-12-04 09:15:05 +0200

    fix config header

--- src/utils/AxiosInstance.ts
+++ src/utils/AxiosInstance.ts
@@ -1,8 +1,22 @@
 import axios from "axios";
+import store from "../redux/store";
 
 export const baseURL = "http://localhost:8080/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
 });
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const accessToken = store.getState()?.auth?.access_token;
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 export default axiosInstance;
